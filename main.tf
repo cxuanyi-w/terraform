bafbafbaab41a0f07964df63b7edffa567247895
cxuanyi-w <cxuanyi.w@gmail.com>
2022-05-31 17:06:59 +0800

    changed variable name for consistency

--- main.tf
+++ main.tf
@@ -29,46 +29,46 @@ module "vpc_module" {
 
 }
 
-# module "s3" {
-#   source = "./modules/s3"
-#   #bucket name should be unique
-#   bucket_name = var.bucket_name
-#   acl_value   = var.acl_value
-# }
+module "s3" {
+  source = "./modules/s3"
+  #bucket name should be unique
+  bucket_name = var.bucket_name
+  acl_value   = var.acl_value
+}
 
-# module "iam" {
-#   source = "./modules/iam"
-# }
+module "iam" {
+  source = "./modules/iam"
+}
 
-# ############ Create Lambda with Local Uploading of Zip File ############
-# module "lambda_with_local_upload" {
-#   source              = "./modules/lambda_local_upload"
-#   input-function_name = var.local_upload_target_lambda_name
-#   target_iam_role     = module.iam.lambda_iam_role
-# }
+############ Create Lambda with Local Uploading of Zip File ############
+module "lambda_with_local_upload" {
+  source              = "./modules/lambda_local_upload"
+  input-function_name = var.local_upload_target_lambda_name
+  target_iam_role     = module.iam.lambda_iam_role
+}
 
-# module "apigw-local-upload" {
-#   source             = "./modules/apigw"
-#   rest_api_name      = var.local_upload_apigw_name
-#   target_lambda_name = var.local_upload_target_lambda_name
-#   target_lambda_arn  = module.lambda_with_local_upload.created_lambda_invoke_arn
-#   target_region      = var.region
-# }
+module "apigw-local-upload" {
+  source             = "./modules/apigw"
+  rest_api_name      = var.local_upload_apigw_name
+  target_lambda_name = var.local_upload_target_lambda_name
+  target_lambda_arn  = module.lambda_with_local_upload.created_lambda_invoke_arn
+  target_region      = var.region
+}
 
-# ############ Create Lambda with S3 Uploading of Zip File ############
-# module "lambda_with_s3_upload" {
-#   source                = "./modules/lambda_s3_upload"
-#   target_function_name  = var.s3_upload_lambda_name
-#   target_iam_role       = module.iam.lambda_iam_role
-#   target_s3_bucket      = module.s3.s3_bucket_name
-#   target_s3_object_name = module.s3.s3_uploaded_object_name
-#   target_s3_object_hash = module.s3.s3_uploaded_object_hash
-# }
+############ Create Lambda with S3 Uploading of Zip File ############
+module "lambda_with_s3_upload" {
+  source                = "./modules/lambda_s3_upload"
+  target_function_name  = var.s3_upload_lambda_name
+  target_iam_role       = module.iam.lambda_iam_role
+  target_s3_bucket      = module.s3.s3_bucket_name
+  target_s3_object_name = module.s3.s3_uploaded_object_name
+  target_s3_object_hash = module.s3.s3_uploaded_object_hash
+}
 
-# module "apigw-s3-upload" {
-#   source             = "./modules/apigw"
-#   rest_api_name      = var.s3_upload_lambda_name
-#   target_lambda_name = var.s3_upload_lambda_name
-#   target_lambda_arn  = module.lambda_with_s3_upload.created_lambda_invoke_arn
-#   target_region      = var.region
-# }
+module "apigw-s3-upload" {
+  source             = "./modules/apigw"
+  rest_api_name      = var.s3_upload_lambda_name
+  target_lambda_name = var.s3_upload_lambda_name
+  target_lambda_arn  = module.lambda_with_s3_upload.created_lambda_invoke_arn
+  target_region      = var.region
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -7,35 +7,35 @@ input-vpc_cidr             = "192.168.0.0/24" # 256 host ip adddresses
 input-instance_tenancy     = "default"
 input-enable_dns_support   = true
 input-enable_dns_hostnames = true
-input-vpc_name             = "cxy-terraform-vpc-1"
+input-vpc_name             = "tf-created-vpc-1"
 # IGW variables
-input-igw_name = "cxy-terraform-vpc-igw"
+input-igw_name = "tf-created-vpc-igw"
 # Public subnet variables
 input-public_subnet_cidr = "192.168.0.0/26" # 128 host ip addresseses, from 192.168.0.0 to 192.168.0.63
-input-public_subnet_name = "cxy-terraform-public-subnet"
-input-public_rt_name     = "cxy-terraform-public-subnet-routetable"
+input-public_subnet_name = "tf-created-public-subnet"
+input-public_rt_name     = "tf-created-public-subnet-routetable"
 # Private subnet 1 variables
 input-private_subnet_cidr_1 = "192.168.0.64/26" # 128 host ip addresseses, from 192.168.0.64 to 192.168.0.127
 input-private_subnet_az_1   = "ap-southeast-1a"
-input-private_subnet_name_1 = "cxy-terraform-private-subnet-1"
-input-private_rt_name_1     = "cxy-terraform-private-subnet-routetable-1"
+input-private_subnet_name_1 = "tf-created-private-subnet-1"
+input-private_rt_name_1     = "tf-created-private-subnet-routetable-1"
 # Private subnet 2 variables
 input-private_subnet_cidr_2 = "192.168.0.128/26" # 128 host ip addresseses, from 192.168.0.128 to 192.168.0.191
 input-private_subnet_az_2   = "ap-southeast-1b"
-input-private_subnet_name_2 = "cxy-terraform-private-subnet-2"
-input-private_rt_name_2     = "cxy-terraform-private-subnet-routetable-2"
+input-private_subnet_name_2 = "tf-created-private-subnet-2"
+input-private_rt_name_2     = "tf-created-private-subnet-routetable-2"
 # Subnet Group for DB
-input-aws_db_subnet_group_name      = "cxy-terraform-private-subnet-group"
-input-aws_db_subnet_group_tags_name = "CXY Terraform Private Subnet Group for DB"
+input-aws_db_subnet_group_name      = "tf-created-private-subnet-group"
+input-aws_db_subnet_group_tags_name = "TF Created Private Subnet Group for DB"
 
 ############ Initiate S3 variables ############
-bucket_name = "cxy-s3-terraform-bucket3" # unique bucket name
+bucket_name = "tf-created-s3-bucket3" # unique bucket name
 acl_value   = "public"                   # acl value, private, public-read, public-read-write, authenticated-rea, aws-exec-read and log-delivery-write
 
 ############ Initiate Local Upload Lambda variables ############
-local_upload_target_lambda_name = "terraform_lambda_upload_from_local"
+local_upload_target_lambda_name = "tf-lambda-upload-from-local"
 local_upload_apigw_name         = "local-upload-lambda-apigw"
 
 ############ Initiate S3 Upload Lambda variables ############
-s3_upload_lambda_name = "terraform_lambda_upload_from_s3"
+s3_upload_lambda_name = "tf-lambda-upload-from-s3"
 s3_upload_apigw_name  = "s3-upload-lambda-apigw"
